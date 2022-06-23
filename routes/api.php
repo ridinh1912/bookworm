161b94655bfafc940010046bbb5c7c38446462e0
Đinh Việt Hoàng <viethoanghnid@gmail.com>
2022-06-23 14:12:57 +0700

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ShopController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/books', [HomeController::class, 'getDiscountBook']);
+Route::get('/recommendedbooks', [HomeController::class, 'getRecommendedBook']);
+Route::get('/popularbooks', [HomeController::class, 'getPopularBook']);
+Route::get('books/sale/{condition?}', [ShopController::class, 'sortByOnSale']);
+Route::get('books/popular/{condition?}', [ShopController::class, 'sortPopularBook']);
+Route::get('books/price/{condition?}', [ShopController::class, 'sortPriceBook']);
+
+
