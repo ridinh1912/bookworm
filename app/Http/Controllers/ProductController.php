0a0de6ed5874a2b1e9fa1789b3ac971c17cf1e7e
Đinh Việt Hoàng <viethoanghnid@gmail.com>
2022-07-04 13:31:13 +0700

    add ProductController

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use App\Models\Review;
+use Illuminate\Http\Request;
+use App\Repositories\ReviewRepository;
+use Illuminate\Queue\NullQueue;
+
+class ProductController extends Controller
+{
+    private ReviewRepository $_reviewRepository;
+    public function __construct(ReviewRepository $reviewRepository)
+    {
+        $this->_reviewRepository = $reviewRepository;
+    }
+    public function getReviewByID(Request $request,$sort="asc")
+    {
+        $perPage = $request->perPage ?? 20;
+        return response($this->_reviewRepository->getReviewByID()->where('book.id',$request->input('book_id'))->orderByRaw("review.review_date {$sort}")->paginate($perPage));
+    }
+    public function getReviewByRating(Request $request,$sort="asc")
+    {
+        $perPage = $request->perPage ?? 20;
+        return response($this->_reviewRepository->getReviewByID()->where('book.id',$request->input('book_id'))->where('review.rating_start','=',$request->input('star'))->orderByRaw("review.review_date {$sort}")->paginate($perPage));
+    }
+    
+}
