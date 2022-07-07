9f839c8810dac209addee69b5a63459d4d49aae3
Đinh Việt Hoàng <viethoanghnid@gmail.com>
2022-07-07 18:33:03 +0700

    add CardItem Component

---
+++ resources/js/components/CardItem.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import { Card } from 'react-bootstrap'
+import COVER from '../../assets/bookcover/CoverPhoto.js'
+
+
+export default function CardItem(props) {
+    const {ele} = props
+    return (
+        <div>
+            <Card style={{ width: '100%', height: '24rem', }} >
+                <Card.Img variant="top" src={ ele.book_cover_photo? COVER[ele.book_cover_photo] : COVER['defaultimg'] } style={{ width: '100%', height: '75%' }} />
+                <Card.Body>
+
+                    <Card.Title className='text-truncate'>{ele.book_title}</Card.Title>
+                    <Card.Text>
+                        <small>{ele.author_name}</small>
+
+                        
+                        
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    )
+}
