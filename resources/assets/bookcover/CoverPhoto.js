a7713ff259e6a462019b499c356fa4e7a18d36f0
Đinh Việt Hoàng <viethoanghnid@gmail.com>
2022-07-07 18:37:35 +0700

    import Image

---
+++ resources/assets/bookcover/CoverPhoto.js
@@ -0,0 +1,15 @@
+const COVER = {
+    book1: require('./book1.jpg').default,
+    book2: require('./book2.jpg').default,
+    book3: require('./book3.jpg').default,
+    book4: require('./book4.jpg').default,
+    book5: require('./book5.jpg').default,
+    book6: require('./book6.jpg').default,
+    book7: require('./book7.jpg').default,
+    book8: require('./book8.jpg').default,
+    book9: require('./book9.jpg').default,
+    book10: require('./book10.jpg').default,
+    defaultimg:  require('./defaultimg.jpg').default,
+    
+}
+export default COVER;
