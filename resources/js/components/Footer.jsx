6ca17dee1983e44004870ccff112ef266311b1af
Đinh Việt Hoàng <viethoanghnid@gmail.com>
2022-07-09 23:31:12 +0700

    edit layout Header, Footer

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -4,12 +4,11 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    
     <Navbar className='footer' expand="lg" bg="light" variant="light">
         <Container className='d-flex flex-column align-items-start'>
             <h5>BOOKWORM</h5>
-            <h6>Address</h6>
-            <h6>Phone</h6>
+            <h6>District 13 - Ho Chi Minh City</h6>
+            <h6>0999988879</h6>
         </Container>
     </Navbar>
   )
--- resources/js/components/NavBar.jsx
+++ resources/js/components/NavBar.jsx
@@ -7,24 +7,22 @@ export default function NavBar() {
     return (
         <>
             <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky='top'>
-                <Container>
+                <Container >
                     <Navbar.Brand href="/">
                         <img src={Logo} alt="Kiwi standing on oval" />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end'>
                         <Nav className="ml-auto">
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/shop">Shop</Nav.Link>
-                            <Nav.Link href="/about">About</Nav.Link>
-                            <Nav.Link href="/cart">Cart</Nav.Link>
-                            <Nav.Link href="/cart">Sign in</Nav.Link>
+                            <Nav.Link href="/"><b>Home</b></Nav.Link>
+                            <Nav.Link href="/shop"><b>Shop</b></Nav.Link>
+                            <Nav.Link href="/about"><b>About</b></Nav.Link>
+                            <Nav.Link href="/cart"><b>Cart</b></Nav.Link>
+                            <Nav.Link href="/cart"><b>Sign in</b></Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-
         </>
-
     )
 }
