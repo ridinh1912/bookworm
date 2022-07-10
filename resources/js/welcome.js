f7bde45c93398c567ba608da2a62092c28960270
Đinh Việt Hoàng <viethoanghnid@gmail.com>
2022-07-11 02:19:58 +0700

    config

--- resources/js/welcome.js
+++ resources/js/welcome.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
+// import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import '../css/app.css';
 import About from './pages/About';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import Product from './pages/product/Product';
 import Shop from './pages/Shop';
 
 
 
 class Welcome extends Component {
+    
     render() {
         return (
                 <Routes>
                     <Route path='/shop' element={<Shop/>} />
                     <Route path='/about' element={<About/>} />
                     <Route path='/' element={<Home/>} />
-                    <Route path='/shop/product' element={<Product/>} />
+                    <Route path='/login' element={<Login/>} />
+                    <Route path="/product/:id" element={<Product/>}/>
                 </Routes>
         );
     }
