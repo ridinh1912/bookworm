a72071653383e63ee7948ce511678067c3317cfc
Đinh Việt Hoàng <viethoanghnid@gmail.com>
2022-07-11 22:55:47 +0700

    import

--- resources/js/store.js
+++ resources/js/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import shopReducer from './features/shopSlice'
+import productReducer from './features/product'
+
 
 export default configureStore({
   reducer: {
-    shopReducer: shopReducer
+    shopReducer: shopReducer,
+    productReducer : productReducer
+
   }
 })
