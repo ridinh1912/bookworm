e4b64f49b64b67bd811c3631e7edcb3e9d84fb02
Đinh Việt Hoàng <viethoanghnid@gmail.com>
2022-07-11 22:56:11 +0700

    add localStorage

--- resources/js/components/NavBar.jsx
+++ resources/js/components/NavBar.jsx
@@ -1,22 +1,40 @@
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import Logo from '../../assets/bookworm_icon.svg'
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+
+
 
 export default function NavBar() {
-    const [checkLogin, setLogin] = React.useState(false);
-
-    
+    const [checkLogin, setLogin] = useState(false);
+    const [firstname, setfirstName] = useState('')
+    const [lastname, setlastName] = useState('')
+    const cartItem = useSelector(state => state.productReducer.cart)
     useEffect(() => {
         const token = localStorage.getItem("token");
-        
-
-        if(token) {
+        const user = localStorage.getItem("user")
+        if (token) {
             setLogin(true);
+        }
+        if (user) {
+        setfirstName(JSON.parse(user).first_name)
+        setlastName(JSON.parse(user).last_name)
         }
 
 
     }, []);
+    const handleLogout = () => {
+        localStorage.clear()
+        window.location.href = '/login'
+    }
+    const navigate=useNavigate()
+    const handleCart = (id) => {
+        navigate(id)
+    }
+
 
     return (
         <>
@@ -28,16 +46,22 @@ export default function NavBar() {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end'>
                         <Nav className="ml-auto">
-                            <Nav.Link href="/"><b>Home</b></Nav.Link>
-                            <Nav.Link href="/shop"><b>Shop</b></Nav.Link>
-                            <Nav.Link href="/about"><b>About</b></Nav.Link>
-                            <Nav.Link href="/cart"><b>Cart</b></Nav.Link>
+                            <Nav.Link onClick={(()=> handleCart('/'))}  style={{ fontSize: '20px' }}><b>Home</b></Nav.Link>
+                            <Nav.Link onClick={(()=> handleCart('/shop'))} style={{ fontSize: '20px' }}><b>Shop</b></Nav.Link>
+                            <Nav.Link onClick={(()=> handleCart('/about'))} style={{ fontSize: '20px' }}><b>About</b></Nav.Link>
+                            <Nav.Link onClick={(()=> handleCart('/cart'))} style={{ fontSize: '20px' }}><b>Cart ({cartItem.counter})</b></Nav.Link>
                             {checkLogin ?
-                                <Nav.Link href="/login"><b>Log out</b></Nav.Link> :
-                                <Nav.Link href="/login"><b>Log in</b></Nav.Link>
+                                (<>
+                                    <NavDropdown className='justify-content-center'  style={{ fontSize: '20px' }} title="&nbsp;&nbsp;&nbsp;Welcome" id="basic-nav-dropdown">
+                                        <NavDropdown.Item  disabled>{firstname} {lastname}</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item onClick={handleLogout} href="/login">Log out</NavDropdown.Item>
+                                    </NavDropdown>
+                                </>) :
+                                <Nav.Link style={{ fontSize: '20px' }} href="/login"><b>Log in</b></Nav.Link>
                             }
                         </Nav>
-                        {checkLogin && <p></p>}
+                       
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
--- resources/js/pages/Login.jsx
+++ resources/js/pages/Login.jsx
@@ -1,92 +1,87 @@
 import React, { useState } from 'react'
 import { Button, Container, Form, Row } from 'react-bootstrap'
-import { Link } from 'react-router-dom';
 import Footer from '../components/Footer'
 import NavBar from '../components/NavBar'
-import { Navigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../features/shopSlice';
 
-// import { useAuth } from '../Auth';
 
 export default function Login(props) {
-    const [email,setEmail]=useState("");
-    const [password,setPassword]=useState("");
-    const [isLoggedIn, setLoggedIn] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const axios = require('axios');
-    const [token, setToken]=useState([]);
+    const [token, setToken] = useState(true);
 
     useEffect(() => {
         localStorage.getItem("token");
-        
-        if(token) {
-            window.location.href = "/";
+        if (token) {
+            // window.location.href = "/";
         }
-    })
 
+    },[])
+    
+    const dispatch=useDispatch()
     async function postLogin(e) {
         e.preventDefault();
         await axios.post("http://127.0.0.1:8000/api/login", {
-            email,password
-        },{
+            email, password
+        }, {
             headers: {
-              'Authorization': `Basic ${token}` 
+                'Authorization': `Bearer ${token}`
             }
-          }).then(result => {
+        }).then(result => { 
             if (result.status === 200) {
-                
-                setLoggedIn(true);
+                dispatch(setUser(result.data.user))
                 setToken(result.data.token)
-                localStorage.setItem("token",result.data.token);
-                
+                localStorage.setItem("token", result.data.token);
+                localStorage.setItem("user", JSON.stringify(result.data.user));
                 window.location.href = "/";
             } else {
                 console.log('err')
             }
         }).catch(e => {
-            
+
             console.log(e);
         });
+        
     }
-
-    // if (isLoggedIn) {
-    //     return <Navigate to={referer} />;
-    // }
-
     return (
-        <div style={{background:'linear-gradient(to bottom, #ffffff 0%, #000066 1500%)'}}>
-            <NavBar/>
-            <br/><br/><br/><br/><br/><br/><br/>
-            <Container style={{width:'600px',backgroundColor:'white',boxShadow: 'rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px'}} className='d-flex justify-content-center' >
+        <>
+            <div style={{ minHeight: 'calc(100vh - 110px)', background: 'linear-gradient(to bottom, #ffffff 0%, #000066 1500%)' }}>
+                <NavBar />
+                <br /><br /><br /><br /><br /><br /><br />
+                <Container style={{ width: '600px', backgroundColor: 'white', boxShadow: 'rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px' }} className='d-flex justify-content-center' >
 
-            <Row style={{paddingTop:'60px',paddingBottom:'60px'}}>
-            
-                <h1 className='d-flex justify-content-center'>Log in</h1>
-                <Form onSubmit={(e) => postLogin(e)}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control name='email' onChange={e=>{ setEmail(e.target.value); }} type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
+                    <Row style={{ paddingTop: '60px', paddingBottom: '60px' }}>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control name='password' onChange={e=>{ setPassword(e.target.value); }} type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button type="submit" variant="secondary">
-                        Log in
-                    </Button>
-                </Form>
-                {/* <Link>Register now</Link> */}
-            </Row>
+                        <h1 className='d-flex justify-content-center'>Log in</h1>
+                        <Form onSubmit={(e) => postLogin(e)}>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control name='email' onChange={e => { setEmail(e.target.value); }} type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
 
-            </Container>
-            <br/><br/><br/><br/><br/><br/><br/><br/>
-            <Footer/>
-        </div>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control name='password' onChange={e => { setPassword(e.target.value); }} type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" label="Check me out" />
+                            </Form.Group>
+                            <Button type="submit" variant="secondary">
+                                Log in
+                            </Button>
+                        </Form>
+                    </Row>
+
+                </Container>
+                <br /><br /><br /><br /><br /><br /><br /><br />
+
+            </div><Footer />
+        </>
     )
 }
